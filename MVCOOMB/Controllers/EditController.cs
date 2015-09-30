5d440d6ac156996fd25997db6ff7bf5386fd11ae
Enrique Thedy <ethedy@gmail.com>
2015-09-29 21:29:25 -0300

    clase 9 final

---
+++ MVCOOMB/Controllers/EditController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+using Entidades;
+using Servicios;
+
+namespace MvcOMB.Controllers
+{
+  [Authorize]
+  public class EditController : Controller
+  {
+    // GET: Edit
+    public ActionResult EditarLibro(string isbn)
+    {
+      ProductServices serv = new ProductServices();
+      Libro libro;
+
+      libro = serv.GetLibroFromIsbn(isbn);
+      if (libro == null)
+        return HttpNotFound("Libro no encontrado");
+
+      return View(libro);
+    }
+
+    [HttpPost]
+    public ActionResult EditarLibro(Libro libro, string isbn13)
+    {
+      ProductServices serv = new ProductServices();
+      Libro libroDominio;
+
+      libroDominio = serv.GetLibroFromIsbn(isbn13);
+
+      if (libroDominio != null)
+      {
+        if (TryUpdateModel(libroDominio, new string[]
+                          {
+                            nameof(libro.ISBN10), nameof(libro.Titulo),
+                            "Autores", "Editorial", "Precio"
+                          }))
+        {
+          if (!serv.ActualizarLibro(libroDominio))
+            return new HttpStatusCodeResult(500);
+        }
+      }
+
+      return View(libroDominio);
+    }
+  }
+}
---
+++ Servicios/ProductServices.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Data;
+using Entidades;
+
+namespace Servicios
+{
+  public class ProductServices
+  {
+    public Libro GetLibroFromIsbn(string isbn)
+    {
+      OMBContext ctx = DB.Contexto;
+
+      return ctx.Libros.Find(isbn);
+    }
+
+    public bool ActualizarLibro(Libro libro)
+    {
+      OMBContext ctx = DB.Contexto;
+
+      try
+      {
+        ctx.MostrarCambios();
+        ctx.Entry(libro).State = EntityState.Modified;
+        ctx.SaveChanges();
+      }
+      catch (Exception)
+      {
+        return false;
+      }
+      return true;
+    }
+  }
+}
